cbf55d4be0292791775cfd9819acc4680870bdb6
tony <tony@123.com>
2017-07-25 10:12:16 +0800

    【ADD】fa.session模型、视图、菜单 odoo模块开发 1

--- odoo/feitas_academy/__manifest__.py
+++ odoo/feitas_academy/__manifest__.py
@@ -24,8 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/session_views.xml',
 
         'views/sale_views.xml',
     ],
